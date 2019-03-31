b84adc38fc45162084c4431f2ca54ee31f1dc373
Jeremias ramirez <jereramirez499@gmail.com>
2019-03-30 21:54:00 -0400

    Agregando funcionalidad de elevar n a la x

--- m.js
+++ m.js
@@ -12,10 +12,18 @@ class M {
 		}
 
 	}
-	
+	elev(a,b){
+		a=parseInt(a);
+		b=parseInt(b);
 
+		if( ( a !== "" ) && ( b !== "" ) || ( a !== undefined ) ){
+			return Math.pow(a,b);
+		}
+		else{
+			console.error("Require 1 value");
+		}
+	} 
 	
-
 }
 let m = new M();
-console.log(m.sum("3","3"));
+console.log(m.elev("2","3"));
