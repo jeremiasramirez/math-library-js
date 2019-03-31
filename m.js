4292811f27995977df981f2bfdddbb421bb995bb
Jeremias ramirez <jereramirez499@gmail.com>
2019-03-30 20:41:59 -0400

    agregando funcionalidad de sumar valores y eliminando archivos innecesario

--- m.js
+++ m.js
@@ -1,6 +1,21 @@
-class m {
+class M {
 
-	sum(a,b){
-		
+	  sum(a,b){
+		a=parseInt(a);
+		b=parseInt(b);
+
+		if( ( a !== "" ) && ( b !== "" ) || (a!==undefined) ){
+			return a + b;
+		}
+		else{
+			console.error("Require 1 value");
+		}
+
 	}
+	
+
+	
+
 }
+let m = new M();
+console.log(m.sum("3","3"));
